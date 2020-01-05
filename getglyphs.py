7be0c601d69ec79d2ff929a72b26816be37dc038
Darky2020 <59534288+Darky2020@users.noreply.github.com>
2020-01-05 17:09:55 +0200

    Update and rename getimages.py to getglyphs.py

--- getglyphs.py
+++ getglyphs.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 import urllib.request
 path = Path()
 num = input("How many glyphs: ")
-i = 13314
+i = 13314 # hex value of first glyph
 for z in range(int(num)):
 	q = str(hex(i+z))[2:]
 	response = requests.get("http://en.glyphwiki.org/glyph/u" + q + "@4.50px.png")
 	try:
 		urllib.request.urlretrieve("http://en.glyphwiki.org/glyph/u" + q + "@4.50px.png","glyhp_" + str(z) + ".png")
 		print("Downloaded glyhp №" + str(z+1))
-	except urllib.error.HTTPError as e:
+	except urllib.error.HTTPError as e: # pass glyph if it doesnt exist
    		if e.code == 404:
    			print("Passing glyph " + str(hex(i+z)[2:]))
    			z -= 1
